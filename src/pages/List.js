3dd7fbbae547723bfec7b1fd5f0e4371618aa950
Deco Oliveira <deco.oliveira@gmail.com>
2019-10-04 15:58:17 -0300

    navigation from list to book with data

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
+import React,{ useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Image, AsyncStorage, StyleSheet } from 'react-native';
+import logo from '../assets/logo.png';
+import SpotList from '../components/SpotList';
 
 export default function List(){
 
-    return <View/>
+    const [techs, setTechs] = useState([]);
 
+    useEffect(()=>{
+        AsyncStorage.getItem('techs').then(storedTechs => {
+            const techsArray = storedTechs.split(',').map(tech => tech.trim()); 
+            setTechs(techsArray);
+        });
+    },[]);
+
+    return (
+        <SafeAreaView style={StyleSheet.container}>
+            <Image source= {logo} style={styles.logo}/>
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo:{
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    }
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, AsyncStorage, KeyboardAvoidingView, Text, Image, StyleSheet, TextInput, 
     Platform, TouchableOpacity } from 'react-native';
 import logo from '../assets/logo.png';
@@ -9,6 +9,14 @@ export default function Login({navigation}){
     const[email,setEmail] = useState('');
     const[techs, setTechs] = useState('');
 
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user){
+                navigation.navigate('List');
+            }
+        });
+    },[]);
+    
     async function handleSubmit(){
         const response = await api.post('/sessions',{
             email
